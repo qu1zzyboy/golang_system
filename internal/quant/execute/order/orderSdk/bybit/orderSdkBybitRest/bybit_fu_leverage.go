8bec3341435dfd8c4c724d923b8e584c808f2164
hhh500 <2282915646@qq.com>
2025-11-13 15:58:40 +0800

    bybit 杠杆测试通过

--- internal/quant/execute/order/orderSdk/bybit/orderSdkBybitRest/bybit_fu_leverage.go
+++ internal/quant/execute/order/orderSdk/bybit/orderSdkBybitRest/bybit_fu_leverage.go
@@ -14,20 +14,19 @@ import (
 	"upbitBnServer/pkg/utils/timeUtils"
 )
 
-var fuLeverageQueryByte = fmt.Appendf(nil, "%s/v5/position/set-leverage", bybitConst.BASE_URL)
+var fuLeverageUrl = fmt.Sprintf("%s/v5/position/set-leverage", bybitConst.BASE_URL)
 
 func (s *FutureRest) DoLeverage(leverage uint8, symbolName string) ([]byte, error) {
 	orig := make([]byte, 0, 128)
-	orig = append(orig, "category=linear"...)
-
-	orig = append(orig, "&buyLeverage="...)
+	orig = append(orig, `{"category":"linear","buyLeverage":"`...)
 	orig = strconv.AppendUint(orig, uint64(leverage), 10)
-	orig = append(orig, "&sellLeverage="...)
+	orig = append(orig, `","sellLeverage":"`...)
 	orig = strconv.AppendUint(orig, uint64(leverage), 10)
-	orig = append(orig, "&symbol="...)
+	orig = append(orig, `","symbol":"`...)
 	orig = append(orig, symbolName...)
+	orig = append(orig, `"}`...)
 
-	r, err := s.addSignDoLeverage(fuLeverageQueryByte, orig)
+	r, err := s.addSignDoLeverage(fuLeverageUrl, orig)
 	if err != nil {
 		return nil, err
 	}
@@ -35,34 +34,30 @@ func (s *FutureRest) DoLeverage(leverage uint8, symbolName string) ([]byte, erro
 	return httpx.DefaultClient.Do(r)
 }
 
-func (s *FutureRest) addSignDoLeverage(urlByte []byte, param []byte) (*httpx.HttpRequest, error) {
+func (s *FutureRest) addSignDoLeverage(urlStr string, param []byte) (*httpx.HttpRequest, error) {
 	// 构建请求头 Header
 	timeStamp := timeUtils.GetNowTimeUnixMilli()
 	header := s.httpHeader.Clone()
 	header.Set(p_TIME_STAMP_KEY, convertx.GetTimeStampMilliStrBy(timeStamp))
 	header.Set(p_RECEIVE_WINDOW_KEY, "5000")
-	// 生成签名数据
-	signData := byteBufPool.AcquireBuffer(byteBufPool.SIZE_128)
+	// 生成签名
+	signData := byteBufPool.AcquireBuffer(byteBufPool.SIZE_256)
+	defer byteBufPool.ReleaseBuffer(signData)
 	*signData = convertx.AppendValueToBytes(*signData, timeStamp) //添加时间戳
 	*signData = append(*signData, s.apiKeyByte...)                //添加apiKey
 	*signData = append(*signData, b_RECEIVE_WINDOW_...)           //添加接收窗口
 	*signData = append(*signData, param...)                       //添加查询参数
+
 	//得到签名结果
 	signResp := byteBufPool.AcquireBuffer(byteBufPool.SIZE_64)
-	defer func() {
-		byteBufPool.ReleaseBuffer(signData)
-		byteBufPool.ReleaseBuffer(signResp)
-	}()
+	defer byteBufPool.ReleaseBuffer(signResp)
 	err := myCrypto.HmacSha256Fast(s.secretByte, *signData, signResp)
 	if err != nil {
 		return nil, err
 	}
 	header.Set(p_SIGNATURE_KEY, string(*signResp))
 	// URL 解析
-	urlByte = append(urlByte, '?')
-	urlByte = append(urlByte, param...)
-	fmt.Println(string(urlByte))
-	parsedURL, err := url.Parse(string(urlByte))
+	parsedURL, err := url.Parse(urlStr)
 	if err != nil {
 		return nil, err
 	}
