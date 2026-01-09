3b3cea56c32d6b202091735fda9fec37a5073c67
Hanbo Li <litterpigger@gmail.com>
2026-01-09 15:38:09 +0800

    debug

--- internal/infra/observe/log/staticLog/static_log.go
+++ internal/infra/observe/log/staticLog/static_log.go
@@ -46,6 +46,14 @@ func setLogDefaultConfig(log *logrus.Logger) {
 func NewLoggerWithLever(cfg Config) *logrus.Logger {
 	log := logrus.New()
 	log.SetOutput(io.Discard) //关闭默认输出 采用钩子方式输出到文件及控制台
+
+	// DEBUG 级别只输出到控制台，不写入文件
+	log.AddHook(&writer.Hook{
+		Writer:    os.Stdout,
+		LogLevels: []logrus.Level{logrus.DebugLevel},
+	})
+
+	// INFO 及以上级别输出到控制台和文件
 	lumberjackOutputInfo := &lumberjack.Logger{
 		Filename:   GetLogFileName(cfg.FileDir, cfg.DateStr, cfg.FileName),
 		MaxSize:    MaxFileSize,
@@ -55,7 +63,7 @@ func NewLoggerWithLever(cfg Config) *logrus.Logger {
 	}
 	log.AddHook(&writer.Hook{
 		Writer:    io.MultiWriter(os.Stdout, lumberjackOutputInfo),
-		LogLevels: []logrus.Level{logrus.DebugLevel, logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel},
+		LogLevels: []logrus.Level{logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel},
 	})
 	//添加error钩子
 	if cfg.NeedErrorHook {
